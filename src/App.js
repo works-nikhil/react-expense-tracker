28cc439abc36d7b76c665b9d29d97dc4f90239a5
works-nikhil <works.nikhil@gmail.com>
2022-03-10 17:44:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./Tasks";
+import AddTask from "./AddTask";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [showAddTask, setShowAddTask] = useState(false)
+
+
+  const [tasks, setTasks] = useState([
+    {
+        "id": 1,
+        "text": "Doctors Appointment",
+        "day": "Feb 5th at 2:30pm",
+        "reminder": true
+    },
+    {
+        "id": 2,
+         "text": "Meeting at School",
+         "day": "Feb 6th at 1:30pm",
+         "reminder": true
+    },
+    {
+      "id": 3,
+       "text": "Meeting at the Club",
+       "day": "Feb 8th at 2:30pm",
+       "reminder": false
+  }
+])
+
+
+// Add Task
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000)+1 //generating a random number
+  console.log(id)
+  const newTask = { id, ...task } // storing new task in the object 'task' along with a random id
+  setTasks([ ...tasks, newTask ]) // changing the state and storing the new task (state) in the 'task' object
+
+
 }
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder that changes boolean value of the task.reminder
+
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) =>
+    task.id === id ?
+      { ...task, reminder:!task.reminder } 
+        : task) )
+}
+
+
+
+// will check if it's true and won't check/return anything if false 
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+      { showAddTask && <AddTask onAdd={addTask} /> }    
+      { tasks.length > 0 ? 
+      (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />)
+         : ('No Tasks to Show') }  
+
+
+    </div>
+  )
+}
+
 
 export default App;
---
+++ src/Task.js
@@ -0,0 +1,16 @@
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task, onDelete, onToggle }) => {
+  return (
+    <div
+        className=
+        {`task ${task.reminder ? 'reminder' : '' }`} // will include reminder to the class only if reminder is true by default
+         onDoubleClick={() => onToggle(task.id) }>
+        <h3>{task.text} <FaTimes style={{ color:'red', cursor:'pointer'}} onClick={() => onDelete(task.id)}/></h3>
+        <p>{task.day}</p>
+      
+    </div>
+  )
+}
+
+export default Task
